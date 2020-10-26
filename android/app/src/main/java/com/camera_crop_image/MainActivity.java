51e4ac8fb00a0ece748db660a068756dd12fb662
Purwanto <purwanto.just1@gmail.com>
2020-10-26 20:11:19 +0700

    init and add all package

---
+++ android/app/src/main/java/com/camera_crop_image/MainActivity.java
@@ -0,0 +1,15 @@
+package com.camera_crop_image;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "camera_crop_image";
+  }
+}
